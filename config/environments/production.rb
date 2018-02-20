3efa0a96d478e45290a7435beebe16cb50865fd1
Intuita-Stefan <stefan.ritchie@intuitaconsulting.com>
2018-02-20 14:43:25 +0000

    Chat maybe?

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.allowed_request_origins = ['https://r5-actioncable-tests.herokuapp.com/',
+                                                 'http://r5-actioncable-tests.herokuapp.com/']
+
+  config.action_cable.url = "wss://sitepoint-actioncable.herokuapp.com/cable"
 end
